e79f02fcfeb0864317d830d9d8efea88482b125c
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-05-20 18:31:03 +0200

    ok

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -4,6 +4,7 @@ export const GalleryList = styled.ul`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
+  justify-content: center; /* Добавлено значение center для выравнивания по центру */
   gap: 5px;
   padding-top: 50px;
   list-style: none;
